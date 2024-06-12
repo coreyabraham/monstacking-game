18aab2f8654bc61848cb99605b4c9563c97b4e45
Corey Abraham <pbv54umpq@relay.firefox.com>
2024-06-12 16:53:19 +1000

    Base Unity Files

---
+++ Assets/Scripts/TestCube.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TestCube : MonoBehaviour
+{
+    private void Start()
+    {
+        print("Initialized TestCube.cs!");
+        // TODO: Hook Cube to events here!
+    }
+}
